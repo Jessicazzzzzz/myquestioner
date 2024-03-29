35f9f5e816cf0c0a1e218149f260e9165a010687
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-03-29 18:37:55 +1100

    chore: commit-lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,35 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:prettier/recommended"
-	],
-	ignorePatterns: ["dist", ".eslintrc.cjs"],
-	parser: "@typescript-eslint/parser",
-	plugins: ["react-refresh"],
-	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-	// plugins:['react','@typescript-eslint'],
-	rules: {
-		"react-refresh/only-export-components": [
-			"warn",
-			{ allowConstantExport: true },
-		],
-		"react/react-in-jsx-scope": 0,
-		semi: 0, // 关闭语句末尾分号
-		"prefer-const": 2, // 开启不变的变量一定要使用const
-		"no-unused-vars": error,
-	},
-};
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }], //should add ".ts" if typescript project,
+    'react/jsx-no-target-blank': 'off', //https://github.com/yannickcr/eslint-plugin-react/jsx-no-target-blank
+  },
+}
