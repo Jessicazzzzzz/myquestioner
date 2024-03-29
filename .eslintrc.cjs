4e707c903b2c985de6f28cef92cc4d06cbe597a6
Jessicazzzzzz <86815288+Jessicazzzzzz@users.noreply.github.com>
2024-03-29 17:32:35 +1100

    add eslint ,prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,25 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    // 'react-refresh/only-export-components': [
-    //   'warn',
-    //   { allowConstantExport: true },
-    // ],
-    semi:0,// 关闭语句末尾分号
-    quotes: [2, 'single'],//使用单引号, 
-    'prefer-const':2,// 开启不变的变量一定要使用const 
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    'space-before-function-paren': 0,
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:prettier/recommended"
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["react-refresh"],
+	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+	// plugins:['react','@typescript-eslint'],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"react/react-in-jsx-scope": 0,
+		semi: 0, // 关闭语句末尾分号
+		"prefer-const": 2, // 开启不变的变量一定要使用const
+		"no-unused-vars": error,
+	},
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports={
+  arrowParents:'avoid',
+  bracketSpacing:true,
+  endofLine:'lf',
+  jsxBracketSameLine:false,
+  proseWrap:'preserve',
+  semi:false,
+  tabWidth:2,
+  singleQuote:true,
+  trailingComma:'es5',
+  printWidth:100,
+  parser:'typescript',
+}
